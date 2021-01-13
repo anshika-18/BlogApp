43a387fade882128fa59169e21257779f0a08a5e
anshika-18 <74226523+anshika-18@users.noreply.github.com>
2021-01-13 16:55:43 +0530

    pre deployment changes

--- index.js
+++ index.js
@@ -20,10 +20,14 @@ app.get('/',(req,res)=>{
     res.send("Welcome to the class");
 });
 
+app.use('*',(req,res,next)=>{
+    res.status(404).json({"msg":"Not found"});
+})
+
 require('./routes/route')(app);
-const Port=3000;
+const Port=process.env.PORT||3000;
 
 app.listen(Port,(err)=>{
     if(err) console.log(err);
     else console.log(`port is live at ${Port}`);
-});
+})
